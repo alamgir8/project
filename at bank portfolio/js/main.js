02b1dfac5da4eaf2969f1c1780d0c58576dc0ad4
alamgir8 <ahossain38900@gmail.com>
2021-01-27 20:32:47 +0600

    some updated in shoping cart

--- at bank portfolio/js/main.js
+++ at bank portfolio/js/main.js
@@ -15,29 +15,6 @@ document.getElementById('deposit-button').addEventListener('click', function () 
    
     const depositNumber = getNumber('deposit-amount');
     document.getElementById('deposit-amount').value = '';
-    
-
-
-
-    
-   
-
-    //for counting deposit amount
-    // const currentDepositAmount = document.getElementById('currentDeposit').innerText;
-    // const currentDepositNumber = parseFloat(currentDepositAmount);
-    // const totalDeposit = depositNumber + currentDepositNumber;
-    // document.getElementById('currentDeposit').innerText = totalDeposit;
-    
-
-
-    //for counting total balance after deposit
-    // const currentBalance = document.getElementById('balanceAmount').innerText;
-    // const currentBalanceAmount = parseFloat(currentBalance);
-    // const totalBalance = depositNumber + currentBalanceAmount;
-    // document.getElementById('balanceAmount').innerText = totalBalance;
-
-
-   
 
     updatedSpanBalance('currentDeposit', depositNumber);
     updatedSpanBalance('currentBalance', depositNumber);
@@ -57,28 +34,11 @@ function updatedSpanBalance(id, depositNumber) {
 
 //for withdraw button handler
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    // const withdrawAmount = document.getElementById('withdraw-amount').value;
-    // const withdrawNumber = parseFloat(withdrawAmount);
-    // document.getElementById('withdraw-amount').value = '';
+  
     const amountNumber = getNumber('withdraw-amount');
     updatedSpanBalance("currentWithdraw", amountNumber);
     updatedSpanBalance('currentBalance', -1*amountNumber);
     document.getElementById('withdraw-amount').value = '';
-
-
-    // for counting withdraw amount
-    // const currentWithdrawAmount = document.getElementById('currentWithdraw').innerText;
-    // const currentDepositNumber = parseFloat(currentWithdrawAmount);
-    // const totalWithdraw = amountNumber + currentDepositNumber;
-    // document.getElementById('currentWithdraw').innerText = totalWithdraw;
-    
-
-
-    // // for counting total balance after deposit
-    // const currentBalance = document.getElementById('balanceAmount').innerText;
-    // const currentBalanceAmount = parseFloat(currentBalance);
-    // const totalBalance = currentBalanceAmount - withdrawNumber;
-    // document.getElementById('balanceAmount').innerText = totalBalance;
 
 })
 
