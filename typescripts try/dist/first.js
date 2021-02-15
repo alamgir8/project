fc680c1047cfb5a647fdebe433e9deb2a1ef3de8
alamgir8 <ahossain38900@gmail.com>
2021-02-15 20:14:55 +0600

    updated typescript

--- typescripts try/dist/first.js
+++ typescripts try/dist/first.js
@@ -15,3 +15,40 @@ function fullName(firstName, lastName) {
 }
 fullName('Alamgir', ' Hossain');
 const firstPersonName = fullName('Hanif', 'Khan');
+let multiply = (x, y) => number; //typescript for arrow function sample
+multiply = (x, y) => x * y;
+multiply = (a, b) => a * b;
+//typescript for array
+let friend = ['hanif', 'alamin']; //for as a sample of Array to call 
+const friendAge = [12, 13, 15, 10];
+friend = ['Aminul', 'Alamin', 'Rasel']; //here no need to make with structure because it already declare in upper
+//typescript for object
+let person;
+//now use that object as much as i can
+person = {
+    name: 'Al-Amin',
+    age: 25,
+    salary: 17,
+    isMale: true
+};
+const salah = {
+    name: 'Salah',
+    age: 29,
+    salary: 2000000,
+    wifeName: 'MagiSalah',
+    club: 'liverpool',
+    isPlaying: true,
+    country: 'Egypt'
+};
+const pogba = {
+    name: 'pogba',
+    age: 29,
+    salary: 2000000,
+    wifeName: 'MagiSalah',
+    club: 'man-utd',
+    isPlaying: true,
+};
+function getBonus(player) {
+    return player.salary * .01;
+}
+getBonus(salah);
--- typescripts try/first.ts
+++ typescripts try/first.ts
@@ -19,14 +19,81 @@ function fullName(firstName:string, lastName: string): string {
 }
 
 
-
-
-
-
-
-
-
-
-
 fullName ('Alamgir',' Hossain');
 const firstPersonName:string = fullName('Hanif', 'Khan');
+
+
+let multiply = (x : number, y: number) => number;   //typescript for arrow function sample
+
+multiply = (x, y) => x*y;
+
+multiply = (a, b) => a*b;
+
+
+
+
+//typescript for array
+
+let friend : string[] =['hanif', 'alamin'] //for as a sample of Array to call 
+const friendAge: number[] = [12, 13, 15, 10];
+
+
+friend = ['Aminul', 'Alamin', 'Rasel']; //here no need to make with structure because it already declare in upper
+
+
+//typescript for object
+
+let person : { //for object sample declare
+    name: string,
+    age:number,
+    salary: number,
+    isMale: boolean
+}
+
+
+//now use that object as much as i can
+person = {
+    name: 'Al-Amin',
+    age:25,
+    salary:17,
+    isMale:true
+}
+
+
+interface Player {
+    name: string,
+    age: number,
+    salary: number,
+    wifeName: string,
+    club: string,
+    isPlaying: boolean,
+    country?: string //? to write for option , if anyone use this player interface he can whether add or not adding county in his object
+
+}
+
+const salah: Player ={
+    name: 'Salah',
+    age: 29,
+    salary: 2000000,
+    wifeName: 'MagiSalah',
+    club: 'liverpool',
+    isPlaying: true,
+    country: 'Egypt'
+
+}
+const pogba: Player ={
+    name: 'pogba',
+    age: 29,
+    salary: 2000000,
+    wifeName: 'MagiSalah',
+    club: 'man-utd',
+    isPlaying: true,
+    
+
+}
+
+function getBonus(player:Player) {
+    return player.salary *.01;
+}
+
+getBonus(salah)
