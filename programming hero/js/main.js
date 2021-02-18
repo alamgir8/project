b503324c2b07fa1924c9122d041ea1fe01a705fb
alamgir8 <ahossain38900@gmail.com>
2021-02-19 01:14:57 +0600

    updated

---
+++ programming hero/js/main.js
@@ -0,0 +1,73 @@
+
+function displayCourseContent() {
+    const content = document.getElementById('display');
+    
+
+    const description = `
+    
+            <div class= ''>
+                <div class="row ">
+                    <div class="col-md-6 d-flex  px-5 pt-2 ">
+                        <div class='card w-100 course-information'>
+                            <div class=' d-flex justify-content-center  align-items-center p-4'>
+                                <div class= 'd-flex col-md-4 justify-content-center  align-items-center'>
+                                <span class= 'icon-thirteen rounded-circle'><i class="fas fa-laptop fa-5x"></i></span>
+                                </div>
+                                <div class='mt-4 '>
+                                    <h4>এইটা কমপ্লিট ওয়েব ডেভেলপমেন্ট এর কোর্স:</h4>
+                                    <p>শূন্য থেকে একজন জুনিয়র ওয়েব ডেভেলপার হিসেবে চাকরি পাওয়ার জন্য যা যা লাগে তার সবকিছুই ধরে ধরে শিখানো হয়েছে এই কোর্সে। সো, এই কোর্স ভালোভাবে ফিনিশ করলে ওয়েব ডেভেলপমেন্ট সম্পর্কে আর কোথাও কোন কোর্স না করেই ইন্টার্ন বা চাকরির চেষ্টা শুরু করে দিতে পারবে।</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex px-5 pt-2">
+                    <div class='card w-100 course-information'>
+                        <div class='d-flex justify-content-center align-items-center p-4'>
+                            <div class= 'd-flex col-md-3 justify-content-center  align-items-center'>
+                            <span class='icon-twelve rounded-circle'><i class="fas fa-user-graduate fa-5x"></i></i></span>
+                            </div>
+                            <div class='mt-4 '>
+                                    <h4>আনলিমিটেড সাপোর্ট:</h4>
+                                    <p>কোর্স চলাকালীন সময়ে ২৪ ঘন্টার মধ্যে তোমার যেকোন প্রশ্নের উত্তর দেয়া হবে (ছুটির দিন বাদে)। অথাৎ প্রথম তিন মাস আমাদের ফেইসবুক গরূপে তোমার সব প্রশ্নের উত্তর দেয়া হবে। এবং জটিল কোন প্রবলেম হলে সেটা যদি ফেইসবুক গরূপে সলভ না হয় তাহলে সপ্তাহে ২/৩ বার গুগল হ্যাংআউট এ এসে তোমার স্কিন শেয়ার করে প্রবলেম সলভ করা হবে। আর কোর্স শেষ হয়ে গেলে (৩মাস পরেও ) তোমাকে লুজ সাপোর্ট দেয়া হবে (২৪ ঘন্টা লিমিট থাকবে না।)</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="row ">
+                    <div class="col-md-6 d-flex px-5 pt-2">
+                        <div class='card w-100 course-information'>
+                            <div class=' d-flex justify-content-center align-items-center p-4'>
+                                <div class= 'd-flex col-md-4 justify-content-center align-items-center'>
+                                <span class= 'icon-thirteen rounded-circle'><i class="fas fa-users fa-5x"></i></i></span>
+                                </div>
+                                <div class='mt-4 '>
+                                    <h4>স্পেশাল ইন্টারভিউ প্রিপারেশন গ্রূপ:</h4>
+                                    <p>যারা ডেইলি ডেইলি কাজ করবে। প্রতিদিন ৬ থেকে ১০ ঘন্টা সময় দিবে। জানপ্রাণ দিয়ে সিরিয়াসভাবে কোর্স নিদৃস্ট সময়ের মধ্যে ফিনিশ করবে। তাদের নিয়ে তৈরি করা SCIC (Super Charged Interviewee Club) । এইটা ৪ থেকে ৬ সপ্তাহের স্পেশাল ট্রেনিং। যেটার জন্য আলাদা কোন ফি নাই। একটাই যোগ্যতা: সিরিয়াস হার্ডওয়ার্ক করে নিদৃস্ট সময়ের মধ্যে কোর্স ফিনিশ করা।</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex px-5 pt-2">
+                    <div class='card w-100 course-information'>
+                        <div class='d-flex justify-content-center align-items-center p-4'>
+                            <div class= 'd-flex col-md-4 justify-content-center align-items-center'>
+                            <span class='icon-twelve rounded-circle'><i class="fas fa-id-card fa-5x fa-fw"></i></span>
+                            </div>
+                            <div class='mt-4 '>
+                                    <h4>চাকরির জন্য সিভি পাঠানো</h4>
+                                    <p>যারা SCIC তে সুযোগ পাবে। এবং সেটা ঠিক মতো শেষ করবে আরো ৪ থেকে ৬ সপ্তাহ লেগে থাকবে। তাদের সিভি আমরা পাঠিয়ে দিবো বিভিন্ন কোম্পানিতে। কোম্পানি হায়ার করবে, কি করবে না: সেটা তাদের সিদ্ধান্ত। আমাদের কাজ হচ্ছে সিভি পৌঁছে দেয়া। আমাদের একজন টিম মেম্বার কাজ করে যাবে তাকে চাকরি পাওয়ার জন্য হেল্প করতে। সো, যতক্ষণ পর্যন্ত তুমি লেগে আছো। আমরা আছি তোমার সাথে।</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+    
+
+                   
+            </div>  
+    
+    `
+
+    content.innerHTML = description;
+   
+ 
+}
