ce0418dbaf9fe9d89116cb7504d58028e4d3f0f9
alamgir8 <ahossain38900@gmail.com>
2021-02-28 16:28:54 +0600

    updated

--- react-practice-app/src/Component/UserCart/UserCart.js
+++ react-practice-app/src/Component/UserCart/UserCart.js
@@ -4,26 +4,11 @@ import React from 'react';
 const UserCart = (props) => {
    
     const user = props.userCart;
-    // const {id, name, gender, language, job, works, email, country, city} = user;
-    
-    console.log(user);
+
     
     return (
         <div>
-                <h1>Added User list :  {user.length}</h1>
-                {
-                    user.map(ur => ur.name)
-                }
-                {/* <h4>ID : {id}</h4>
-                <h4>Name : {name}</h4>
-                <h4>Gender : {gender}</h4>
-                <h4>Language : {language}</h4>
-                <h4>Job : {job}</h4>
-                <h4>Company : {works}</h4>
-                <h4>Email : {email}</h4>
-                <h4>Country : {country}</h4>
-                <h4>City : {city}</h4> */}
-           
+                <h1>Added User list :  {user.length}</h1>          
 
         </div>
     );
